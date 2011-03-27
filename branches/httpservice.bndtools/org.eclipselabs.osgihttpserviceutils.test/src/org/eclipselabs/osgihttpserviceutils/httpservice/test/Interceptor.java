f0ebaf6fa8fac14e5d5b65567002c3741c57c869
FvUndRaecher@googlemail.com <FvUndRaecher@googlemail.com@1f94c7f6-8a86-3a15-b3c6-75cbb3b0676c>
2011-03-27 20:04:54 +0000

    Fix demo intercptor removed return false

--- branches/httpservice.bndtools/org.eclipselabs.osgihttpserviceutils.test/src/org/eclipselabs/osgihttpserviceutils/httpservice/test/Interceptor.java
+++ branches/httpservice.bndtools/org.eclipselabs.osgihttpserviceutils.test/src/org/eclipselabs/osgihttpserviceutils/httpservice/test/Interceptor.java
@@ -2,36 +2,28 @@ package org.eclipselabs.osgihttpserviceutils.httpservice.test;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
+
 import org.eclipselabs.osgihttpserviceutils.httpservice.HttpRequestInterceptor;
 import org.eclipselabs.osgihttpserviceutils.httpservice.RequestService;
 
 public class Interceptor implements HttpRequestInterceptor {
 
-  private RequestService requestService;
+	private RequestService requestService;
 
-  @Override
-	public boolean afterRequest() {
-		try {
-			requestService.getRequestContext().getResponse().getWriter()
-					.println("After");
-		} catch (IOException exp) {
-			throw new RuntimeException(exp);
-		}
-		return false;
+	@Override
+	public void afterRequest() throws IOException, ServletException {
+		requestService.getRequestContext().getResponse().getWriter()
+				.println("After");
 	}
 
-  @Override
-	public boolean beforeRequest() {
-		try {
-			requestService.getRequestContext().getResponse().getWriter()
-					.println("Before");
-		} catch (IOException exp) {
-			throw new RuntimeException(exp);
-		}
-		return false;
+	@Override
+	public void beforeRequest() throws IOException, ServletException {
+		requestService.getRequestContext().getResponse().getWriter()
+				.println("Before");
 	}
 
-  public void setRequestService(RequestService requestService) {
+	public void setRequestService(RequestService requestService) {
 		this.requestService = requestService;
 	}
 
