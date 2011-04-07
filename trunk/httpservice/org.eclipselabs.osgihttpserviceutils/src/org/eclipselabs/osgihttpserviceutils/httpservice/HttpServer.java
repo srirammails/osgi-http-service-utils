2ff5abd378e6634bde85045e5e082e48f33424a4
FvUndRaecher@gmail.com <FvUndRaecher@gmail.com@1f94c7f6-8a86-3a15-b3c6-75cbb3b0676c>
2011-04-07 12:21:16 +0000

    add service methods for custom service properties for a HTTP Service instance.

--- trunk/httpservice/org.eclipselabs.osgihttpserviceutils/src/org/eclipselabs/osgihttpserviceutils/httpservice/HttpServer.java
+++ trunk/httpservice/org.eclipselabs.osgihttpserviceutils/src/org/eclipselabs/osgihttpserviceutils/httpservice/HttpServer.java
@@ -1,5 +1,7 @@
 package org.eclipselabs.osgihttpserviceutils.httpservice;
 
+import java.util.Map;
+
 /**
  * The HTTP server which provides one OSGi HTTP service. The server can be
  * configured via a Jetty XML configuration file. The configuration file must
@@ -24,6 +26,13 @@ public interface HttpServer {
 	int getPort();
 
 	/**
+	 * Return a map of custum service proeprties.
+	 * 
+	 * @return the map of custom service properties not null.
+	 */
+	Map<Object, Object> getServiceProperties();
+
+	/**
 	 * The symbolic name of the server, the HTTP server has a symbolic name
 	 * which is unique.
 	 * 
@@ -41,13 +50,28 @@ public interface HttpServer {
 	HttpServer port(int httpPort);
 
 	/**
+	 * Add a Map of custom service properties to the HTTP Service.
+	 * @param serviceProperties the service properties not null.
+	 * @return the actual http server instance.
+	 */
+	HttpServer serviceProperties(Map<?, ?> serviceProperties);
+	
+	/**
+	 * Set a custom service property to the HTTP Service.
+	 * @param key the service property key.
+	 * @param value the value of the service property.
+	 * @return the actual service instance.
+	 */
+	HttpServer serviceProperty(Object key, Object value);
+	
+	/**
 	 * Setter for the HTTP port. The port should not be used.
 	 * 
 	 * @param port
 	 *            the port for the HTTP services of the HTTP server.
 	 */
 	void setPort(int port);
-
+	
 	/**
 	 * Start a HTTP server in the platform.
 	 * 
